a153aab19aa2507718dd5c888769972af65ec781
Cory D <cjdoebel@gmail.com>
2025-01-02 16:50:57 -0500

    uplaoded new files, forgot i had to make a repo before i started

---
+++ src/classes/Wheel.ts
@@ -0,0 +1,40 @@
+// Wheel class that defines the properties of a wheel
+class Wheel {
+  // Declare properties of the Wheel class using private access modifier
+  private diameter: number;
+  private tireBrand: string;
+
+  // Constructor for the Wheel class
+  constructor(diameter: number = 18, tireBrand: string = "GoodYear") {
+    this.diameter = diameter;
+    this.tireBrand = tireBrand;
+  }
+
+  // Getter methods for the properties of the Wheel class
+  get getDiameter(): number {
+    return this.diameter;
+  }
+
+  // Setter
+  set getDiameter(diameter: number) {
+    this.diameter = diameter;
+  }
+
+  // Setter method for the diameter property
+  get getTireBrand(): string {
+    return this.tireBrand;
+  }
+
+  // Setter
+  set setTireBrand(tireBrand: string) {
+    this.tireBrand = tireBrand;
+  }
+
+  // Method to print the details of the wheel
+  printDetails(): string {
+    return `Diameter: ${this.diameter}, Tire Brand: ${this.tireBrand}`;
+  }
+}
+
+// Export the Wheel class
+export default Wheel;
